ebbc229682749424700628ad1cea891b28e84ad2
georgitinchev <g.tinchev@student.fontys.nl>
2024-09-07 21:03:08 +0300

    Database Helper Class

---
+++ WebAppTelebid/Helpers/DatabaseHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data.SqlClient;
+using WebAppTelebid.Interfaces;
+
+public class DatabaseHelper : IDatabaseHelper
+{
+    private readonly string _connectionString;
+
+    public DatabaseHelper(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public SqlConnection GetConnection()
+    {
+        return new SqlConnection(_connectionString);
+    }
+
+    public void OpenConnection(SqlConnection connection)
+    {
+        try
+        {
+            if (connection.State != System.Data.ConnectionState.Open)
+            {
+                connection.Open();
+            }
+        }
+        catch (SqlException ex)
+        {
+            throw new Exception("Database connection error.", ex);
+        }
+    }
+
+    public void CloseConnection(SqlConnection connection)
+    {
+        try
+        {
+            if (connection.State != System.Data.ConnectionState.Closed)
+            {
+                connection.Close();
+            }
+        }
+        catch (SqlException ex)
+        {
+            Console.WriteLine($"Error closing connection: {ex.Message}");
+        }
+    }
+}
