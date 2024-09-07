f5dc2f763f436297064cc2d3cbf50a2cf943fd24
georgitinchev <g.tinchev@student.fontys.nl>
2024-09-07 21:02:14 +0300

    Program.cs class starting up the server

---
+++ WebAppTelebid/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WebAppTelebid.Interfaces;
+using WebAppTelebid.Repositories;
+using WebAppTelebid.Services;
+
+namespace WebAppTelebid
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            string connectionString = LoadConnectionString();
+            IDatabaseHelper databaseHelper = new DatabaseHelper(connectionString);
+            IUserRepository userRepository = new UserRepository(databaseHelper);
+            IAuthService authService = new AuthService(userRepository);
+            RequestHandler requestHandler = new RequestHandler(authService, userRepository);
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:8080/");
+            listener.Start();
+            Console.WriteLine("Server is running on http://localhost:8080/");
+
+            while (true)
+            {
+                try
+                {
+                    HttpListenerContext context = await listener.GetContextAsync();
+                    requestHandler.HandleRequest(context);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+        }
+
+        private static string LoadConnectionString()
+        {
+            var jsonString = File.ReadAllText("appsettings.json");
+            var jsonDocument = JsonDocument.Parse(jsonString);
+            return jsonDocument.RootElement
+                .GetProperty("ConnectionStrings")
+                .GetProperty("DefaultConnection")
+                .GetString();
+        }
+    }
+}
